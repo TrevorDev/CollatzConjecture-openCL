1d612527547bafe6f765dc762dad3d0814a3f9ec
ahnh <ahn.heesung@gmail.com>
2013-11-24 19:48:29 -0500

    update

--- omp_oneToAll.c
+++ omp_oneToAll.c
@@ -3,13 +3,13 @@
 #include <omp.h>
 #include <time.h>
 #include <inttypes.h>
+#define DATA_SIZE (524288)
 
 int parseArgs(int argc, char * argv[]);
 void InitializeData();
 
-int arraySize = 1;
+int depth = 1;
 int rep = 1;
-int *seed = NULL;
 int *input, *output;
 
 int main(int argc, char *argv[])
@@ -91,18 +91,14 @@ int main(int argc, char *argv[])
 
 int parseArgs(int argc, char * argv[]){
     int repSet = 0;
-    int sizeSet = 0;
+    int depthSet = 0;
     for(int i=0;i<argc;i++){
         int num = atoi(argv[i]+1);
         switch( argv[i][0] ) 
         {
-        case 's':
-            seed = calloc(1,sizeof(int));
-            *seed=num;
-            break;
-        case 'n':
-            arraySize=num;
-            sizeSet=1;
+        case 'd':
+            depth=num;
+            depthSet=1;
             break;
         case 'r':
             rep=num;
@@ -112,12 +108,11 @@ int parseArgs(int argc, char * argv[]){
             break;
         }
     }
-    if(!sizeSet || !repSet || arraySize<=0 || rep<=0){
-        printf("Invalid arguments, expected 2 or 3 args:\n");
+    if(!depthSet || !repSet || rep<=0||depth<0||depth>19){
+        printf("Invalid arguments, expected 2 args:\n");
         printf("r - number of repititions greater than 0(to increase runtime)\n");
-        printf("n - numbers to compute between 1 and 524288\n");
-        printf("s - random seed (optional leave out to seed by time)\n");
-        printf("Example execution ./openCL-allToOne r3 n524288 s3 > output.txt\n");
+        printf("d - depth of reverse collatz tree to print out between 1 and 19\n");
+        printf("Example execution ./openCL-allToOne r1 d8 > output.txt\n");
         return 1;
     }
     return 0;
@@ -126,20 +121,8 @@ int parseArgs(int argc, char * argv[]){
 void InitializeData()
 {
   int i;
-  input = (int *) malloc (arraySize*sizeof(int));
-  output = (int *) calloc (arraySize, sizeof(int));
-  
-  if(seed==NULL)
-  {
-    srand(time(NULL));
-  }
-  else
-  {
-    srand(*seed);
-  }
+  input = (int *) calloc (DATA_SIZE, sizeof(int));
+  output = (int *) calloc (DATA_SIZE, sizeof(int));
 
-  for (i = 0; i < arraySize; i++)
-  {
-      input[i] = (rand()%100000000)+1;
-  }
+  input[0] = 1;
 }
